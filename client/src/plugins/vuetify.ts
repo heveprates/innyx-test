2e308f61134aee81a10c229fabefcea627c65ac4
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-02 03:31:39 -0300

    feat: adicionado i18n para datepicker

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -5,11 +5,14 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+import { VDatePicker } from "vuetify/labs/VDatePicker";
+import DateFnsAdapter from "@date-io/date-fns";
+import { enUS, ptBR } from "date-fns/locale";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
@@ -17,10 +20,31 @@ export default createVuetify({
     themes: {
       light: {
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+          primary: "#1867C0",
+          secondary: "#5CBBF6",
         },
       },
     },
   },
-})
+  components: {
+    VDatePicker,
+  },
+  date: {
+    locale: {
+      en: enUS,
+      br: ptBR,
+    },
+    adapter: DateFnsAdapter,
+  },
+  locale: {
+    messages: {
+      br: {
+        datePicker: {
+          ok: "Ok",
+          cancel: "Cancelar",
+          header: "Escolha a data",
+        },
+      },
+    },
+  },
+});
