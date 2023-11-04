8700fe327de761302abda37091593452e17225b3
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-04 18:31:00 -0300

    feat: criando os metodos da interface de CategoryService

---
+++ api/app/Contracts/Services/CategoryServiceInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Contracts\Services;
+
+use App\DataTransferObjects\Category\CategoryCreateDTO;
+use App\DataTransferObjects\Category\CategoryUpdateDTO;
+
+interface CategoryServiceInterface
+{
+    public function paginate(int $currentPage, int $perPage = 10, string $search = null);
+
+    public function findById(int $id);
+
+    public function create(CategoryCreateDTO $productCreateDTO);
+
+    public function updateById(int $id, CategoryUpdateDTO $productUpdateDTO);
+
+    public function deleteById(int $id);
+}
