3897867ab7744f6004394ce2a212521529f96300
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-04 16:35:44 -0300

    feat: adicionado casts para o model Product

--- api/app/Models/Product.php
+++ api/app/Models/Product.php
@@ -5,6 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string name
+ * @property string description
+ * @property float price
+ * @property \DateTimeInterface date_validity
+ * @property string image
+ * @property int category_id
+ */
 class Product extends Model
 {
     use HasFactory;
@@ -16,6 +25,11 @@ class Product extends Model
         'date_validity',
         'image',
         'category_id'
+    ];
+
+    protected $casts = [
+        'price' => 'float',
+        'date_validity' => 'datetime:Y-m-d',
     ];
 
     public function category(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
