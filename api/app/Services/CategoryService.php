4cc9137bbe0c590e205f7be2cb13223412e34ad2
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-04 18:36:57 -0300

    feat: criando o service da category

---
+++ api/app/Services/CategoryService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\Services\CategoryServiceInterface;
+use App\DataTransferObjects\Category\CategoryCreateDTO;
+use App\DataTransferObjects\Category\CategoryUpdateDTO;
+use App\Models\Category;
+
+class CategoryService implements CategoryServiceInterface
+{
+
+    public function __construct(
+        private readonly Category $categoryModel
+    ) {
+    }
+    public function paginate(int $currentPage, int $perPage = 12, string $search = null)
+    {
+        $query = $this->categoryModel->newQuery();
+        if ($search) {
+            $query->where('name', 'like', "%{$search}%");
+        }
+        return $query->paginate(perPage: $perPage, page: $currentPage);
+    }
+
+    public function findById(int $id): Category
+    {
+        return $this->categoryModel->newQuery()->findOrFail($id);
+    }
+
+    public function create(CategoryCreateDTO $categoryCreateDTO)
+    {
+        $requestInput = [
+            'name' => $categoryCreateDTO->name,
+        ];
+        return $this->categoryModel->newQuery()->create($requestInput);
+    }
+
+    public function updateById(int $id, CategoryUpdateDTO $categoryUpdateDTO)
+    {
+        $category = $this->findById($id);
+        $requestInput = [];
+        if ($categoryUpdateDTO->name !== null) {
+            $requestInput['name'] = $categoryUpdateDTO->name;
+        }
+        return $category->update($requestInput);
+    }
+
+    public function deleteById(int $id)
+    {
+        $category = $this->findById($id);
+        return $category->delete();
+    }
+}
