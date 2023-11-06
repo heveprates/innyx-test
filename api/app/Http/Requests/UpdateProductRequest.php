3fbb7c07a1a1bd7dd434ee14825c73bdb50fe414
Hevellyn Prates <hevellynprates15@gmail.com>
2023-11-05 23:36:41 -0300

    fix: retirando obrigatoriedade de todos os campos do UpdateProductRequest

--- api/app/Http/Requests/UpdateProductRequest.php
+++ api/app/Http/Requests/UpdateProductRequest.php
@@ -4,5 +4,15 @@ namespace App\Http\Requests;
 
 class UpdateProductRequest extends StoreProductRequest
 {
-
+    public function rules(): array
+    {
+        return [
+            'name' => 'nullable|string|max:50',
+            'description' => 'nullable|string|max:200',
+            'price' => 'nullable|numeric|min:0',
+            'date_validity' => 'nullable|date|after:today',
+            'image' => 'nullable|image',
+            'category_id' => 'nullable|exists:categories,id'
+        ];
+    }
 }
